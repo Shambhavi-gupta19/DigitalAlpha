49d448bcb4433d3337eb9ef4a243d739e308718a
Shambhavi-gupta19 <shambhavi070695@gmail.com>
2018-04-05 16:56:29 +0530

    Create ques3.py

---
+++ 4thApril/ques3.py
@@ -0,0 +1,8 @@
+A = {5, 3, 8, 6, 1}
+B = {1, 5, 3, 4, 2}
+
+print("Union : ",A.union(B))
+print("Intersection : ",A.intersection(B))
+print("Set Difference : ",A.difference(B))
+print("Maximum value of set A and B : ",max(A.union(B)))
+print("Minimum value of set A and B : ",min(A.union(B)))
