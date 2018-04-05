ea01587d62b5c5ed0f84c7f9ddb9cc2f910029ed
Shambhavi-gupta19 <shambhavi070695@gmail.com>
2018-04-05 14:26:59 +0530

    Update ques1.py

--- 30thMarch/ques1.py
+++ 30thMarch/ques1.py
@@ -1,19 +1,20 @@
-fo = open("foo.txt", "w")
-l=[]
-n=int(input("Enter no of places u want to enter:"))
-for i in range(n):
-    fo.write(input("Enter place name , population and area with spaces"))
-    fo.write("\n")
-fo.close()
-fo=open("foo.txt","r")
-value=fo.readlines()
-for i in value:
-    l.append(i.rstrip('\n'))
-    l.sort()
-fo.close()
-fo = open("foo.txt", "w")
-print(len(l))
-for i in range(0,len(l)):
-    fo.write(l[i])
-    fo.write("\n")
-fo.close()
+import re
+n=0
+d=0
+u=0
+s=0
+str=input("Enter a string")
+for i in range(0,len(str)):
+    if(re.match(r"[~\!@#\$%\^&\*\(\)_\+{}\":;'\[\]]", str[i])):
+        s=s+1
+    elif(str[i].isdigit()):
+        d=d+1
+    elif(str[i].isupper()):
+        u=u+1
+    elif(str[i].islower()):
+        n=n+1
+print("No of letters:",n+u)
+print("No of digits:",d)
+print("No of letters in uppercase:",u)
+print("No of letters in lowercase:",n)
+print("No of special characters:",s)
