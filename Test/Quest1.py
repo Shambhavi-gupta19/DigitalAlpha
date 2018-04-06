54ee34967d64f4e228fde9e80ae74f3313383d2d
Shambhavi-gupta19 <shambhavi070695@gmail.com>
2018-04-06 15:56:25 +0530

    Create Quest1.py

---
+++ Test/Quest1.py
@@ -0,0 +1,51 @@
+#Question1:
+
+import re
+sen=[]
+v=0
+u=0
+l=0
+s=0
+r=0
+str=input("Enter a paragraph with atleast 4 sentences : ")
+
+#print the paragraph:
+print("Paragraph : \n",str)
+
+#Updating the middle sentence:
+sen=str.split(".")
+sen[int((len(sen)-1)/2)]="UST Global specializes in Healthcare, Retail & Consumer Goods, Banking & Financial Services, Telecom,Media &Technology, Insurance, Transportation & Logistics and Manufacturing & Utilities"
+l=len(sen)
+for i in range(l):
+    print(sen[i],'.')
+
+#print no of vowels:
+for i in range(l):
+    for c in sen[i]:
+        if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u'):
+            v+=1
+        elif(c.isupper()):
+            u+=1
+        elif(c.islower()):
+            l+=1
+        elif(re.match(r"[~\!@#\$%\^&\*\(\)_\+{}\":;'\[\]]", c)):
+            s+=1
+print("No of vowels : ",v)
+print("No of uppercase charcters : ",u)
+print("No of lowercase characters : ",l)
+print("No of special characters : ",s)
+
+#removing special characters:
+
+#swap last and first sentence:
+temp=sen[0]
+sen[0]=sen[len(sen)-1]
+sen[len(sen)-1]=temp
+l=len(sen)
+print("Updated Paragraph:\n")
+for i in range(l):
+    print("{}.".format(sen[i]))
+    
+#===================================================================================
+    
+    
