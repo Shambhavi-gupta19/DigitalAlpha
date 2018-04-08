3952cc3ec85eb0263dd45e1d613d753490e5055f
Shambhavi-gupta19 <shambhavi070695@gmail.com>
2018-04-09 00:26:54 +0530

    Update ques1.py

--- April5/ques1.py
+++ April5/ques1.py
@@ -2,11 +2,15 @@
 
 
 d=0
-for i in range(11,20):
+s=0
+print("Armstrong no in the range of 1 to 1000 are : ")
+for i in range(0,1001):
     n=i
     d=0
+    s=0
     while(n!=0):
-        d=i%10
+        d=n%10
         n=int(n/10)
-        d+=d*d*d
-    print(d,i,n)
+        s+=d*d*d
+    if(i==s):
+        print(i)
